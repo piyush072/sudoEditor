facbe7bc7fb496e464e63ff3fe551f4f05f35893
Piyush Ranjan <piyushranjan072@gmail.com>
2018-11-04 10:21:47 +0530

    minor changes

--- script.js
+++ script.js
@@ -5,28 +5,29 @@ var fileName = "";
 var canvas2 = document.getElementById("canvas2");
 var ct = canvas2.getContext("2d");
 function newfn(){
-ct.clearRect(0,0,canvas2.width,canvas2.height)
-var inp = document.getElementById("inp").value;
-var x=document.getElementById("x").value;
-var y=document.getElementById("y").value;
-var z = document.getElementById("z").value;
-var clr = document.getElementById("clr");
-var fstyle = document.getElementById("fstyle").value;
-ct.fillStyle = clr.value;
-ct.font = z+"px "+fstyle;
-ct.fillText(inp,x,y);
+      ct.clearRect(0,0,canvas2.width,canvas2.height)
+      var inp = document.getElementById("inp").value;
+      var x=document.getElementById("x").value;
+      var y=document.getElementById("y").value;
+      var z = document.getElementById("z").value;
+      var clr = document.getElementById("clr");
+      var fstyle = document.getElementById("fstyle").value;
+      ct.fillStyle = clr.value;
+      ct.font = z+"px "+fstyle;
+      ct.fillText(inp,x,y);
 };
 
 $(document).ready(function() {
-  $('#merge').on("click",function(e){var inp = document.getElementById("inp").value;
-  var x=document.getElementById("x").value;
-  var y=document.getElementById("y").value;
-  var z = document.getElementById("z").value;
-  var clr = document.getElementById("clr");
-  var fstyle = document.getElementById("fstyle").value;
-  ctx.fillStyle = clr.value;
-  ctx.font = z+"px "+fstyle;
-  ctx.fillText(inp,x,y);
+  $('#merge').on("click",function(e){
+      var inp = document.getElementById("inp").value;
+      var x=document.getElementById("x").value;
+      var y=document.getElementById("y").value;
+      var z = document.getElementById("z").value;
+      var clr = document.getElementById("clr");
+      var fstyle = document.getElementById("fstyle").value;
+      ctx.fillStyle = clr.value;
+      ctx.font = z+"px "+fstyle;
+      ctx.fillText(inp,x,y);
 
   });
   $("#download-btn").on("click", function(e) {
