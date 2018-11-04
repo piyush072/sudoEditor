1c33057c1f89c0ef1cbc37970b02dda68aac1a1d
Piyush Ranjan <piyushranjan072@gmail.com>
2018-11-04 10:07:11 +0530

    Added Font Style

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ var x=document.getElementById("x").value;
 var y=document.getElementById("y").value;
 var z = document.getElementById("z").value;
 var clr = document.getElementById("clr");
+var fstyle = document.getElementById("fstyle").value;
 ct.fillStyle = clr.value;
-ct.font = z+"px Arial";
+ct.font = z+"px "+fstyle;
 ct.fillText(inp,x,y);
 };
 
@@ -22,8 +23,9 @@ $(document).ready(function() {
   var y=document.getElementById("y").value;
   var z = document.getElementById("z").value;
   var clr = document.getElementById("clr");
+  var fstyle = document.getElementById("fstyle").value;
   ctx.fillStyle = clr.value;
-  ctx.font = z+"px Arial";
+  ctx.font = z+"px "+fstyle;
   ctx.fillText(inp,x,y);
 
   });
